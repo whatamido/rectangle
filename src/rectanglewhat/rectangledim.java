32e37befac04d80a3195107f49cf1138a5cce4dd
whatamido <42977140+whatamido@users.noreply.github.com>
2018-10-02 14:29:24 -0400

    Add files via upload

---
+++ src/rectanglewhat/rectangledim.java
@@ -0,0 +1,20 @@
+package rectanglewhat;
+
+import java.util.Scanner;
+
+public class rectangledim 
+{
+	
+	public static void main(String[] args)
+	{
+		Scanner.in = new Scanner(System.in);
+		System.out.print();
+		
+	}
+	
+	
+	
+	
+	
+	
+}
